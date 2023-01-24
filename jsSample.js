14e03ed034af0efb60a7942bbfb3541eab7608ea
Sonico Samedy <sonicosamedy01@gmail.com>
2023-01-24 14:40:54 -0500

    First Commit

---
+++ jsSample.js
@@ -0,0 +1,5 @@
+
+console.log("------------------");
+console.log("------------------");
+conso.log("tico");
+
---
+++ pythonSample.py
@@ -0,0 +1,26 @@
+print("----------------------------------------------------------")
+print("tico")
+txt = "I love you Mama"
+print(txt[-4:])
+t=['tico', 'tica', 'Aliyaah']
+
+""" 
+"""
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+p = 3
+
+def tico(p, g):
+    if p == 2:
+        print("i love you Mama")
+    else:
+        print("prove your  love")
+
+
+for i in t:
+    print(t)
+
+
+
